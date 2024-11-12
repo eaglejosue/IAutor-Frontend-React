4a1018300167c6b699490a65acc9f71241da3549
Juliano Miquelleto <julianomiquelleto@gmail.com>
2024-11-12 11:51:03 -0300

    empty component

--- src/pages/home/home-logged.tsx
+++ src/pages/home/home-logged.tsx
@@ -4,10 +4,11 @@ import './home-logged.scss'
 import { AuthenticatedUserModel } from '../../common/models/authenticated.model';
 import { homelogged } from '../../assets/svg';
 import { Button } from 'react-bootstrap';
+import EmptyHomeLogged from './sections/empty-logged.section';
 
 const HomeLogged = () => {
   const user = AuthenticatedUserModel.fromLocalStorage();
-
+  
   return (
     <div className="d-flex" style={{ height: "100vh" }}>
       <Sidebar navItem="home" />
@@ -25,71 +26,51 @@ const HomeLogged = () => {
         <main className="main">
           <div className="container-fluid">
             <div className="row m-5">
-              <div className="col-4">
-                <strong>
-                  <h3>Bem vindo, {user?.firstname + " " + user?.lastname}</h3>
-                </strong>
-                <p>Comece criando uma primeira história.</p>
-              </div>
-              <div className="col-8 rectangle-right-admin p-4">
-                  <img
-                    className="img-fluid mb-2"
-                    src={homelogged}
-                    alt="button home logged"
-                  ></img>
-                  <p>
-                    <strong>Faça uma Degustação</strong>
-                  </p>
-                  <p>
-                    Relembre e escreva um lindo livro de memórias e <br></br>
-                    momentos, Experimente!
-                  </p>
-                  <Button
-                    variant=" btn-custom-gray-2"
-                    className=" rounded-5  f-14  p-3"
-                  >
-                    <strong>Começar</strong>
-                  </Button>
-                </div>
-             
+              <EmptyHomeLogged user={user}></EmptyHomeLogged>
             </div>
 
-            <div className='row '>
-              <div className='col-12'>
+            <div className="row ">
+              <div className="col-12">
+                <div className="row  home-explore m-5 bg-white p-5 border rounded">
+                  <div className="col-12 mb-4">
+                    <h4>
+                      <strong>Explore o IAutor</strong>
+                    </h4>
+                  </div>
 
-              
-
-                    <div className="row  home-explore m-5 bg-white p-5 border rounded">
-                            <div className='col-12 mb-4'><h4><strong>Explore o IAutor</strong></h4></div>
-                        
-                            <div className='col-5 explore-left p-5'>
-                                <span><strong>Saiba mais sobre o IAutor</strong></span>
-                                <p className='mt-2'>
-                                Conheça nossa história, valores <br></br>e propósitos.</p>
-                                <Button
-                                variant=" btn-secondary"
-                                className=" rounded-5  f-14  p-3"
-                              >
-                                <strong>Clique aqui</strong>
-                              </Button> 
-                            </div>
-                            <div className='col-2 '></div>
-                            <div className='col-5 explore-right p-5 '>
-                                <span><strong>FAQ</strong></span>
-                                <p className='mt-2'>
-                                Encontre respostas rápidas para<br></br> suas dúvidas mais comuns.</p>
-                                <Button
-                                variant=" btn-secondary"
-                                className=" rounded-5  f-14  p-3"
-                              >
-                                <strong>Clique aqui</strong>
-                              </Button> 
-                            </div>
-                      </div>
-              
+                  <div className="col-5 explore-left p-5">
+                    <span>
+                      <strong>Saiba mais sobre o IAutor</strong>
+                    </span>
+                    <p className="mt-2">
+                      Conheça nossa história, valores <br></br>e propósitos.
+                    </p>
+                    <Button
+                      variant=" btn-secondary"
+                      className=" rounded-5  f-14  p-3"
+                    >
+                      <strong>Clique aqui</strong>
+                    </Button>
+                  </div>
+                  <div className="col-2 "></div>
+                  <div className="col-5 explore-right p-5 ">
+                    <span>
+                      <strong>FAQ</strong>
+                    </span>
+                    <p className="mt-2">
+                      Encontre respostas rápidas para<br></br> suas dúvidas mais
+                      comuns.
+                    </p>
+                    <Button
+                      variant=" btn-secondary"
+                      className=" rounded-5  f-14  p-3"
+                    >
+                      <strong>Clique aqui</strong>
+                    </Button>
+                  </div>
+                </div>
               </div>
-              </div>
-
+            </div>
           </div>
         </main>
       </section>
